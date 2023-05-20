bfdadc5708a84e86974b50efd9d43388e5efe962
C M Mohammed Roshan <72293660+ochufy@users.noreply.github.com>
2023-05-20 21:24:43 +0530

    Update anonymizer_app.py

--- anonymizer_app.py
+++ anonymizer_app.py
@@ -6,6 +6,8 @@ from presidio_analyzer import AnalyzerEngine, Pattern, PatternRecognizer
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 from datetime import datetime
+import wordmap
+import replace_with_synonym
 
 st.title("Data Anonymizer")
 
@@ -19,52 +21,68 @@ if uploaded_file is not None:
     st.write(df)
 
     # Define the regex pattern in a Presidio `Pattern` object:
-    numbers_pattern = Pattern(name="numbers_pattern", regex="[A-Z]{5}[0-9]{4}[A-Z]{1}", score=0.5)
+    pan_number_pattern = Pattern(name="pan_number_pattern", regex="[A-Z]{5}[0-9]{4}[A-Z]{1}", score=0.5)
 
     # Define the recognizer with one or more patterns
-    number_recognizer = PatternRecognizer(supported_entity="PAN_NUMBER", patterns=[numbers_pattern])
+    pan_number_recognizer = PatternRecognizer(supported_entity="PAN_NUMBER", patterns=[pan_number_pattern])
 
-    # Analyzer output
+        # Define the regex pattern in a Presidio `Pattern` object:
+    twitter_mention_pattern = Pattern(name="twitter_mention_pattern", regex="@(\\w+)", score=0.5)
+
+    # Define the recognizer with one or more patterns
+    twitter_mention_recognizer = PatternRecognizer(supported_entity="TWITTER_MENTION", patterns=[twitter_mention_pattern])
+
+    # # Analyzer output
     analyzer = AnalyzerEngine()
-    analyzer.registry.add_recognizer(number_recognizer)
+    analyzer.registry.add_recognizer(pan_number_recognizer)
+    analyzer.registry.add_recognizer(twitter_mention_recognizer)
+
 
     # Anonymizer output
-    anonymizer = AnonymizerEngine()
+    # anonymizer = AnonymizerEngine()
 
     # Define anonymization operators
-    operators = {
-        "LOCATION": OperatorConfig("replace", {"new_value": "<LOCATION>"}),
-        "PERSON": OperatorConfig("replace", {"new_value": "<PERSON>"}),
-        "PHONE_NUMBER": OperatorConfig(
-            "mask",
-            {
-                "type": "mask",
-                "masking_char": "*",
-                "chars_to_mask": 7,
-                "from_end": True,
-            },
-        ),
-        "EMAIL_ADDRESS": OperatorConfig(
-            "mask",
-            {
-                "chars_to_mask": 7,
-                "type": "mask",
-                "from_end": False,
-                "masking_char": "*"
-            }
-        ),
-        "TITLE": OperatorConfig("redact", {}),
-    }
+    # operators = {
+    #     "LOCATION": OperatorConfig("replace", {"new_value": "<LOCATION>"}),
+    #     "PERSON": OperatorConfig("replace", {"new_value": "<PERSON>"}),
+    #     "PHONE_NUMBER": OperatorConfig(
+    #         "mask",
+    #         {
+    #             "type": "mask",
+    #             "masking_char": "*",
+    #             "chars_to_mask": 7,
+    #             "from_end": True,
+    #         },
+    #     ),
+    #     "EMAIL_ADDRESS": OperatorConfig(
+    #         "mask",
+    #         {
+    #             "chars_to_mask": 7,
+    #             "type": "mask",
+    #             "from_end": False,
+    #             "masking_char": "*"
+    #         }
+    #     ),
+    #     "TITLE": OperatorConfig("redact", {}),
+    # }
 
     # Create a new column "Anonymized Tweet" with the anonymized tweet
     anonymized_tweets = []
     for tweet in df["Tweet Content"]:
+        print("original: ", tweet)
         analyzer_results = analyzer.analyze(text=tweet, language="en")
-        anonymized_results = anonymizer.anonymize(
-            text=tweet, analyzer_results=analyzer_results, operators=operators
-        )
-        anonymized_tweet = anonymized_results.text
-        anonymized_tweets.append(anonymized_tweet)
+        for result in analyzer_results:
+            # print(tweet[result.start : result.end])
+            tweet = tweet.replace(tweet[result.start : result.end], wordmap.returnString(tweet[result.start : result.end]))
+            tweet = replace_with_synonym.replace_adjectives_with_synonyms(tweet)
+
+        print("wordmapped: ", tweet)
+        # anonymized_results = anonymizer.anonymize(
+        #     text=tweet, analyzer_results=analyzer_results, operators=operators
+        # )
+        # anonymized_tweet = anonymized_results.text
+        # anonymized_tweets.append(anonymized_tweet)
+        anonymized_tweets.append(tweet)
     df["Anonymized Tweet"] = anonymized_tweets
 
     # Replace the "Tweet Content" column with the "Anonymized Tweet" column
@@ -75,12 +93,25 @@ if uploaded_file is not None:
     st.write("PII identified tweets")
     st.write(df)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
     # k-anonymity
     # Identify the sensitive attributes
     sensitive_attributes = ['User Id', 'Name', 'Twitter Username', 'User Bio', 'Profile URL']
 
     # Determine the K value
-    K = 5
+    K = 3
 
     # Identify the quasi-identifiers
     quasi_identifiers = ['Tweet Location', 'User Followers', 'User Following', 'User Account Creation Date', 'Tweet Posted Time (UTC)']
@@ -128,7 +159,17 @@ if uploaded_file is not None:
     # Generalize or suppress the values of the quasi-identifiers in each group
     generalized_data = []
     for name, group in groups:
+        # print(name, group)
+
+        # st.write("name:")
+        # st.write(name)
+
+        # st.write("group:")
+        # st.write(group)
+
         generalized_group = group.copy()
+
+
         for col in quasi_identifiers:
             if col == 'Tweet Location':
                 # Generalize Tweet Location to broader geographic regions
@@ -142,24 +183,36 @@ if uploaded_file is not None:
             elif col == 'Tweet Posted Time (UTC)':
                 # Generalize User Account Creation Date to month and year
                 generalized_group[col] = group[col].apply(lambda x: generalize_account_creation_date(x))
+        
+        # st.write("generalized group:")
+        # st.write(generalized_group)
+
         generalized_data.append(generalized_group)
 
     # Create a new DataFrame with the anonymized quasi-identifiers and the non-sensitive attribute Tweet Content
     anonymized_data = pd.concat(generalized_data)[['Tweet Location', 'User Followers', 'User Following', 'User Account Creation Date', 'Tweet Content', 'Tweet Posted Time (UTC)']]
-
-    # Verify that each group satisfies the K-Anonymity condition and merge groups if necessary
+    # st.write("anonymized data:")
+    # st.write(anonymized_data)
+    # # Verify that each group satisfies the K-Anonymity condition and merge groups if necessary
     k_anonymized_data = []
     for name, group in anonymized_data.groupby(quasi_identifiers):
+        # st.write("name(k):")
+        # st.write(name)
+        # st.write("group(k):")
+        # st.write(group)
+        # st.write("length of group: ", len(group))
         if len(group) < K:
             # Merge the group with the nearest group that satisfies the K-Anonymity condition
             merged_group = pd.concat([group, anonymized_data.loc[anonymized_data.groupby(quasi_identifiers).groups[name]].iloc[0:K-len(group)]])
             k_anonymized_data.append(merged_group)
         else:
             k_anonymized_data.append(group)
+        # st.write("k-anonymized data:")
+        # st.write(k_anonymized_data)
 
     # Concatenate all the K-Anonymized groups into a final anonymized DataFrame
     df = pd.concat(k_anonymized_data)
     df = df.rename(columns={'Tweet Posted Time (UTC)': 'Tweet Posted Date'})
-    df = df.drop_duplicates()
-    st.write("K-Anonymized Dataset")
-    st.write(df)
+    # df = df.drop_duplicates()
+    # st.write("K-Anonymized Dataset")
+    # st.write(df)
