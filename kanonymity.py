d222b56efd5cb1134f99511d592929cdb5c78a7b
C M Mohammed Roshan <72293660+ochufy@users.noreply.github.com>
2023-06-22 10:31:45 +0530

    Update kanonymity.py

--- kanonymity.py
+++ kanonymity.py
@@ -1,5 +1,5 @@
+import numpy as np
 import pandas as pd
-# import numpy as np
 
 # data = {
 #     'Id': [1, 2, 3, 4, 5],
@@ -8,6 +8,9 @@ import pandas as pd
 # }
 
 # df = pd.DataFrame(data)
+
+# sensitive_attribute = 'Id'
+# quasi_identifiers = ['Likes', 'Retweets']
 
 def k_anonymity(df, k, sensitive_attribute, quasi_identifiers):
     anonymized_df = pd.DataFrame()
@@ -23,15 +26,13 @@ def k_anonymity(df, k, sensitive_attribute, quasi_identifiers):
     
     for i in range(0, len(shuffled_df), block_size):
         block = shuffled_df[i:i+block_size].copy()  # Extract a k-block
-        for column in quasi_identifiers:
-            if column in df.columns:
-                block[column] = block[column].sample(frac=1).reset_index(drop=True)  # Shuffle quasi identifier within the block
+        
+        # Shuffle the sensitive attribute within the block
+        block[sensitive_attribute] = np.random.permutation(block[sensitive_attribute].values)
+        
         shuffled_blocks.append(block)
     
     return pd.concat(shuffled_blocks, ignore_index=True)
 
-# k = 3
-# sensitive_attribute = 'Id'
-# quasi_identifiers = ['Likes', 'Retweets']
-# anonymized_df = k_anonymity(df, k, sensitive_attribute, quasi_identifiers)
-# print(anonymized_df)
+# df2 = k_anonymity(df, 3, sensitive_attribute, quasi_identifiers)
+# print(df2)
